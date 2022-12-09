2b0c018ca5566a87a697ab556519b48dd978e30a
asinchenko <wonderfulsanchez@gmail.com>
2022-12-09 12:42:27 +0600

    useLogin hook

--- dashboard/src/pages/Login.jsx
+++ dashboard/src/pages/Login.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from 'react'
-
+import {useLogin} from '../services/useLogin'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {login, error, isLoading} = useLogin()
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(email, password)
+
+        await login(email, password)
     }
   return (
     <div className="flex justify-center">
@@ -24,7 +27,8 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}    
             />
-            <button>Login!</button>
+            <button disabled={isLoading}>Login!</button>
+            {error && <div>{error}</div>}
         </form>
     </div>
   )
--- dashboard/src/services/useLogin.js
+++ dashboard/src/services/useLogin.js
@@ -2,17 +2,17 @@ import React, {useState} from 'react';
 import {useAuthContext} from './useAuthContext';
 import axios from 'axios'
 
-export const useSignup = () => {
+export const useLogin = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
     const {dispatch} = useAuthContext();
 
 
-    const signup = async(email, password) => {
+    const login = async(email, password) => {
         setIsLoading(true);
         setError(null);
 
-        await axios.post('http://localhost:4000/api/user/signup', {
+        await axios.post('http://localhost:4000/api/user/login', {
             email, password
         }).then(res => {
             let result = res.data;
@@ -25,5 +25,5 @@ export const useSignup = () => {
             setError(e.response.data.error)
     });
     } 
-    return {signup, isLoading, error}
+    return {login, isLoading, error}
 }
